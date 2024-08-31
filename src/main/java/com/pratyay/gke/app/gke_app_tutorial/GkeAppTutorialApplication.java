b4ebb1242ec5d602ec5fc9ffde80a31efa6bde7f
pratyay-das1989 <pratyaydas.1989@live.com>
2024-08-31 19:10:13 +0530

    First Logical commit
    Spring boot structure is created and committed

---
+++ src/main/java/com/pratyay/gke/app/gke_app_tutorial/GkeAppTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.pratyay.gke.app.gke_app_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GkeAppTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GkeAppTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pratyay/gke/app/gke_app_tutorial/GkeAppTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratyay.gke.app.gke_app_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GkeAppTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
